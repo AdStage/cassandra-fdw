aa9cb01d1b5505a5740bb821933cbe2ba44fffbc
G Gordon Worley III <gworley3@gmail.com>
2018-05-11 20:32:11 +0000

    support utf8 in map values

--- cassandra-fdw/cassandra_provider.py
+++ cassandra-fdw/cassandra_provider.py
@@ -431,7 +431,7 @@ class CassandraProvider:
                 elif isinstance(value, OrderedMapSerializedKey):
                     dict_values = {}
                     for i in value:
-                        dict_values[str(i)] = str(value[i])
+                        dict_values[str(i)] = value[i].encode('utf8');
                     line[column_name] = json.dumps(dict_values)
                 else:
                     line[column_name] = value
